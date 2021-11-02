3fe0cdc3c8dacc5f34683fb76652f59c2797a3d3
Dominik Gogola <dominik.gogola@clowd9.com>
2021-11-02 21:24:08 +0100

    - minor changes before merge request

--- main.go
+++ main.go
@@ -1,22 +1,21 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"github.com/86soft/healthyro-recipes/adapters/dao"
 	"github.com/86soft/healthyro-recipes/app"
-	"github.com/86soft/healthyro-recipes/app/command"
-	"github.com/86soft/healthyro-recipes/app/query"
 	"github.com/86soft/healthyro-recipes/ports"
 	"github.com/86soft/healthyro/recipe"
+	"github.com/rs/zerolog"
+	"google.golang.org/grpc"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"net/http"
+	"log"
+	"net"
 	"os"
 )
 
 func main() {
-	ctx := context.Background()
 
 	//dsn := os.Getenv("db_conn")
 	dsn := os.Getenv("db_conn")
@@ -24,8 +23,15 @@ func main() {
 	if err != nil {
 		panic("cannot connect to db")
 	}
-
-	app := app.NewApplication(dao.NewRecipeRepository(db))
-	srv := ports.NewGrpcServer(app)
-	recipe.RegisterRecipeServiceServer()
+	logger := zerolog.New(os.Stderr)
+	a := app.NewApplication(dao.NewRecipeRepository(db))
+	srv := ports.NewRecipeServer(a)
+	port := os.Getenv("PORT")
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%s", port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	recipe.RegisterRecipeServiceServer(grpcServer, srv)
+	logger.Fatal().Msg(grpcServer.Serve(lis).Error())
 }
--- ports/rpc_server.go
+++ ports/rpc_server.go
@@ -12,16 +12,16 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-type GrpcServer struct {
+type RecipeServer struct {
 	app app.Application
 	pb.UnimplementedRecipeServiceServer
 }
 
-func NewGrpcServer(application app.Application) GrpcServer {
-	return GrpcServer{app: application}
+func NewRecipeServer(application app.Application) RecipeServer {
+	return RecipeServer{app: application}
 }
 
-func (s GrpcServer) ListRecipes(ctx context.Context, req *pb.ListRecipesRequest) (*pb.ListRecipesResponse, error) {
+func (s RecipeServer) ListRecipes(ctx context.Context, req *pb.ListRecipesRequest) (*pb.ListRecipesResponse, error) {
 	recipes, err := s.app.Queries.ListRecipes.Handle(ctx)
 	if err != nil {
 		return nil, status.Errorf(codes.InvalidArgument, "error occurred while querying all recipes")
@@ -29,7 +29,7 @@ func (s GrpcServer) ListRecipes(ctx context.Context, req *pb.ListRecipesRequest)
 	return &pb.ListRecipesResponse{Recipes: Helpers.MapRecipesToProto(recipes)}, nil
 }
 
-func (s GrpcServer) GetRecipe(ctx context.Context, req *pb.GetRecipeRequest) (*pb.GetRecipeResponse, error) {
+func (s RecipeServer) GetRecipe(ctx context.Context, req *pb.GetRecipeRequest) (*pb.GetRecipeResponse, error) {
 	q := query.NewGetRecipeById(req.GetUuid())
 
 	recipe, err := s.app.Queries.GetRecipeById.Handle(ctx, q)
@@ -39,7 +39,7 @@ func (s GrpcServer) GetRecipe(ctx context.Context, req *pb.GetRecipeRequest) (*p
 	return &pb.GetRecipeResponse{Recipe: Helpers.MapRecipeToProto(&recipe)}, nil
 }
 
-func (s GrpcServer) CreateRecipe(ctx context.Context, req *pb.CreateRecipeRequest) (*pb.CreateRecipeResponse, error) {
+func (s RecipeServer) CreateRecipe(ctx context.Context, req *pb.CreateRecipeRequest) (*pb.CreateRecipeResponse, error) {
 	cmd := command.NewCreateRecipe(req.GetTitle(), req.GetDescription(), req.GetExternalLink())
 
 	id, err := s.app.Commands.CreateRecipe.Handle(ctx, cmd)
@@ -50,7 +50,7 @@ func (s GrpcServer) CreateRecipe(ctx context.Context, req *pb.CreateRecipeReques
 	return &pb.CreateRecipeResponse{Uuid: id.GetID().String()}, nil
 }
 
-func (s GrpcServer) UpdateRecipeTitle(ctx context.Context, req *pb.UpdateRecipeTitleRequest) (*common.Empty, error) {
+func (s RecipeServer) UpdateRecipeTitle(ctx context.Context, req *pb.UpdateRecipeTitleRequest) (*common.Empty, error) {
 	cmd := command.NewUpdateRecipeTitle(req.GetUuid(), req.GetTitle())
 
 	err := s.app.Commands.UpdateRecipeTitle.Handle(ctx, cmd)
@@ -61,7 +61,7 @@ func (s GrpcServer) UpdateRecipeTitle(ctx context.Context, req *pb.UpdateRecipeT
 	return &common.Empty{}, nil
 }
 
-func (s GrpcServer) UpdateRecipeDescription(ctx context.Context, req *pb.UpdateRecipeDescriptionRequest) (*common.Empty, error) {
+func (s RecipeServer) UpdateRecipeDescription(ctx context.Context, req *pb.UpdateRecipeDescriptionRequest) (*common.Empty, error) {
 	cmd := command.NewUpdateRecipeDescription(req.GetUuid(), req.GetDescription())
 
 	err := s.app.Commands.UpdateRecipeDescription.Handle(ctx, cmd)
@@ -72,7 +72,7 @@ func (s GrpcServer) UpdateRecipeDescription(ctx context.Context, req *pb.UpdateR
 	return &common.Empty{}, nil
 }
 
-func (s GrpcServer) UpdateRecipeExternalLink(ctx context.Context, req *pb.UpdateRecipeExternalLinkRequest) (*common.Empty, error) {
+func (s RecipeServer) UpdateRecipeExternalLink(ctx context.Context, req *pb.UpdateRecipeExternalLinkRequest) (*common.Empty, error) {
 	cmd := command.NewUpdateRecipeExternalLink(req.GetUuid(), req.GetExternalLink())
 
 	err := s.app.Commands.UpdateRecipeExternalLink.Handle(ctx, cmd)
@@ -83,7 +83,7 @@ func (s GrpcServer) UpdateRecipeExternalLink(ctx context.Context, req *pb.Update
 	return &common.Empty{}, nil
 }
 
-func (s GrpcServer) DeleteRecipe(ctx context.Context, req *pb.DeleteRecipeRequest) (*common.Empty, error) {
+func (s RecipeServer) DeleteRecipe(ctx context.Context, req *pb.DeleteRecipeRequest) (*common.Empty, error) {
 	cmd := command.NewDeleteRecipe(req.GetUuid())
 
 	err := s.app.Commands.DeleteRecipe.Handle(ctx, cmd)
